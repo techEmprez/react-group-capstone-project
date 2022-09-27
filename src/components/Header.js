9f0b6f0db9c935b4b23b4530e451ae41b381db2d
David-Kasilia <davidkasilia001@gmail.com>
2022-09-28 00:04:15 +0300

    Fix Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavLink from './NavLink';
+import Logo from '../assets/capstone.png'
 
 
  const Header = () => {
   return (
       <div className="Header">
-      <img className="navLogo" src= "./assets/capstone.png" alt="NavLogo" />
+      <img className="navLogo" src={Logo} alt="NavLogo" />
       <NavLink />
     </div>
   )
