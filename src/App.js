f0c0fe6640a5928ecb2a77551aaec7a1da79c2a2
David-Kasilia <davidkasilia001@gmail.com>
2022-09-27 23:51:30 +0300

    Added Route to Rockets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Rockets'
 
 const App = () => {
   return (
    <Router>
      <Header />
+     <Routes>
+      <Route path="/" element={<Rockets />} />
+      </Routes>
    </Router>
   )
 }
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Rockets = () => {
   return (
     <div>
-      
+      Rockets
     </div>
   )
 }
