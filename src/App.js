e3ee508327cd5c586dcaf5abf570764df2f5b03e
David-Kasilia <davidkasilia001@gmail.com>
2022-09-27 23:57:22 +0300

    Added My Profile Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
      <Routes>
       <Route path="/" element={<Rockets />} />
       <Route path="/missions" element={<Missions />} />
+      <Route path="/profile" element={<MyProfile />} />
       </Routes>
    </Router>
   )
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MyProfile = () => {
   return (
     <div>
-      
+      My Profile
     </div>
   )
 }
