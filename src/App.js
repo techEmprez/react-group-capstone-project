45081c113f8894c40050b6b417f09a2dcf01b23d
David-Kasilia <davidkasilia001@gmail.com>
2022-09-27 23:47:18 +0300

    Import Header to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>App</div>
+   <Router>
+     <Header />
+   </Router>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavLink from './NavLink';
-import Logo from '../assets/capstone';
+
 
  const Header = () => {
   return (
-      <div>Header
-      <img className='navLogo' src= {Logo} alt="NavLogo" />
+      <div className="Header">
+      <img className="navLogo" src= "./assets/capstone.png" alt="NavLogo" />
+      <NavLink />
     </div>
   )
 }
