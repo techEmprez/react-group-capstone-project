049e98df1c19df5da28f6c8ae246c022d81a4286
David-Kasilia <davidkasilia001@gmail.com>
2022-09-28 18:31:33 +0300

    Basic Structure created

--- src/components/MissionInfo.js
+++ src/components/MissionInfo.js
@@ -1,34 +1,34 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import '../App.css'
+import '../App.css';
 
-const MissionInfo = (props) => {
-  return (
-    <div className="mission-container">
-      <table className="mission-table">
-        <tbody>
+const MissionInfo = (props) => (
+  <div className="mission-container">
+    <table className="mission-table">
+      <tbody>
         <tr className="table-head">
-        <th className="mission-head">{props.missionTitle}</th>
+          <th className="mission-head">{props.missionTitle}</th>
           <th className="mission-head">{props.missionDesc}</th>
           <th className="mission-head">{props.missionStatus}</th>
         </tr>
         <tr className="mission-data">
-            <td className="mission1">{props.name}</td>
-            <td className="mission1">{props.description}</td>
-            <td className="mission1">{props.status}</td>
-            <td><button className="btn">Join Mission</button></td>
+          <td className="mission1">{props.name}</td>
+          <td className="mission1">{props.description}</td>
+          <td className="mission1">{props.status}</td>
+          <td><button type="button" className="btn">Join Mission</button></td>
 
         </tr>
-        </tbody>
-        </table>  
+      </tbody>
+    </table>
 
-    </div>
-  )
-}
+  </div>
+);
 
-MissionInfo.defaultProps ={
-    missionTitle : "Missions",
-    missionDesc : "Description",
-    missionStatus : "Status"
-}
+MissionInfo.defaultProps = {
+  missionTitle: 'Missions',
+  missionDesc: 'Description',
+  missionStatus: 'Status',
+};
 
-export default MissionInfo
+export default MissionInfo;
