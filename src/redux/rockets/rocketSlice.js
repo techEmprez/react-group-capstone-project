542d0cfefbbbe37e8baa805a72a3594dab203716
techEmprez <choiceosobor@gmail.com>
2022-09-29 06:26:38 +0100

    fetch data from api

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -3,5 +3,20 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 const baseURL = 'https://api.spacexdata.com/v3/rockets';
 
 export const fetchRocketsData = createAsyncThunk(
-    
+    'rockets/fetchRocketsData',
+  async () => {
+    const response = await fetch(baseURL);
+
+    const data = await response.json();
+
+    const rocketsData = data.map((rocket) => ({
+      id: rocket.rocket_id,
+      name: rocket.rocket_name,
+      description: rocket.description,
+      images: rocket.flickr_images,
+      reserved: false,
+    }));
+
+    return rocketsData;
+  },
 )
