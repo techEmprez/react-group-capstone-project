9eae246e91fa70dcd1e83b65e04732311f517d60
techEmprez <choiceosobor@gmail.com>
2022-09-29 06:25:11 +0100

    export fetchData func for rockets

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -1,3 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const baseURL = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRocketsData = createAsyncThunk(
+    
+)
