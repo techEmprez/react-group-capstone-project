97e331f6bfac9cb0206c5ea7688dc6ca392c2a50
techEmprez <choiceosobor@gmail.com>
2022-09-29 10:31:51 +0100

    fetch data from space X

--- src/redux/rockets/rocketSlice.js
+++ src/redux/rockets/rocketSlice.js
@@ -4,19 +4,38 @@ const baseURL = 'https://api.spacexdata.com/v3/rockets';
 
 export const fetchRocketsData = createAsyncThunk(
     'rockets/fetchRocketsData',
-  async () => {
-    const response = await fetch(baseURL);
+    async () =>
+    {
+        const response = await fetch(baseURL);
 
-    const data = await response.json();
+        const data = await response.json();
 
-    const rocketsData = data.map((rocket) => ({
-      id: rocket.rocket_id,
-      name: rocket.rocket_name,
-      description: rocket.description,
-      images: rocket.flickr_images,
-      reserved: false,
-    }));
+        const rocketsData = data.map((rocket) => ({
+            id: rocket.rocket_id,
+            name: rocket.rocket_name,
+            description: rocket.description,
+            images: rocket.flickr_images,
+            reserved: false,
+        }));
 
-    return rocketsData;
+        return rocketsData;
+    },
+);
+
+const initialState = [];
+
+export const rocketSlice = createSlice({
+  name: 'rocket',
+  initialState,
+  reducers: {
+    reserveRocket: (state, action) => state.map((rocket) => {
+      if (rocket.id === action.payload) {
+        return { ...rocket, reserved: !rocket.reserved };
+      }
+      return rocket;
+    }),
   },
-)
+  extraReducers: {
+    [fetchRocketsData.fulfilled]: (_state, action) => action.payload,
+  },
+});
