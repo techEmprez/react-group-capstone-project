fe8ac31731a24b42b43ba990a5e527a90e5b6e78
techEmprez <choiceosobor@gmail.com>
2022-09-29 11:27:42 +0100

    create routes in navlink

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
 import NavLink from './components/NavLink';
 import RocketContainer from './Components/Rocket/RocketContainer';
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <NavLink />
+      <Routes>
+        <Route exact path="/" element={<RocketContainer />} />
+      </Routes>  
+    </div>
   )
 }
 
