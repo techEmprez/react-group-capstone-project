d26df2e913ecc235dbdf2e80f7109ab1b34f997f
techEmprez <choiceosobor@gmail.com>
2022-09-30 17:17:31 +0100

    link rockets to myprofile

--- src/components/Missions/MyProfile.js
+++ src/components/Missions/MyProfile.js
@@ -1,28 +1,38 @@
-/* eslint-disable no-console */
-/* eslint-disable max-len */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { ListGroup, Container } from 'react-bootstrap';
 
 const MyProfile = () => {
   const joinedMission = useSelector((state) => state.allMissions.missionsList);
+  // eslint-disable-next-line max-len
+  const filterJoinedMissions = joinedMission.filter((joinedMissionList) => joinedMissionList.isMissionJoined);
   console.log(joinedMission);
 
-  const filterJoinedMissions = joinedMission.filter((joinedMissionList) => joinedMissionList.isMissionJoined);
-  console.log(filterJoinedMissions);
+  const rockets = useSelector((state) => state.rocket);
+  const filteredRockets = rockets.filter((rocket) => rocket.reserved === true);
 
   return (
-    <div className="my-profile">
-      <hr />
-      { filterJoinedMissions.length === 0 ? <h4 className="myMissions">You Have No Missions Added</h4> : <h4 className="myMissions">My Missions</h4> }
-      <table className="myMissionRow">
-        <tbody>
-          { filterJoinedMissions.map((mission) => (
-            <tr key={mission.mission_id}><td className="myMissionsList">{mission.mission_name}</td></tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-
+    <Container className="d-flex justify-content-center mt-5">
+      <ListGroup className="w-25 me-5">
+        <h2>My Missions</h2>
+        {filterJoinedMissions.length === 0 ? (
+          <p className="text-danger">Please would you like to join the mission with David?</p>
+        ) : (
+          filterJoinedMissions.map((mission) => (
+            <ListGroup.Item key={mission.mission_id}>{mission.mission_name}</ListGroup.Item>
+          ))
+        )}
+      </ListGroup>
+      <ListGroup className="w-25">
+        <h2>My Rockets</h2>
+        {filteredRockets.length === 0 ? (<p className="text-danger">Hey! Kindly pick a rocket.</p>
+        ) : (
+          filteredRockets.map((rocket) => (
+            <ListGroup.Item key={rocket.id}>{rocket.name}</ListGroup.Item>
+          ))
+        )}
+      </ListGroup>
+    </Container>
   );
 };
 
